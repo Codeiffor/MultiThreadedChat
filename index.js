845f7649dac80db14f702d3e3e12b6f603080fc3
codeiffor <sid96@protonmail.com>
2019-03-01 02:52:57 +0530

    jwt implemented  on user login

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const methods = require('./methods.js')
+
 const logger = require('morgan')
 const errorhandler = require('errorhandler')
 const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
-const methods = require('./methods.js')
+const cookieParser = require('cookie-parser');
 
 const client = new MongoClient('mongodb://localhost:27017')
 const app = express()
 
+app.use(cookieParser('secret'));
 app.use(logger('dev'))
 app.use(bodyParser.json())
 
 client.connect((err) => {
-    if(err) process.exit(1)
+    if (err) process.exit(1)
     const db = client.db('blog')
     const userdata = db.collection('users')
-    // const blogdata = db.collection('blogdata')
-    methods.passcollection(userdata)
+    const blogdata = db.collection('userblogs')
 
-    app.post('/signup', methods.signup)
-    app.post('/login', methods.login)
+    app.post('/signup', (req, res) => {
+        methods.signup(req, res, userdata)
+    })
+    app.post('/login', (req, res) => {
+        methods.login(req, res, userdata)
+    })
+    app.get('/user/:user', methods.auth, (req, res) => {
+        methods.user(req, res, blogdata)
+    })
 
     app.use(errorhandler())
     app.listen(3000)
--- methods.js
+++ methods.js
@@ -1,19 +1,21 @@
-let userdata
-module.exports = {
-    passcollection : (userd) => {
-        userdata = userd
-    },
+const jwt = require('jsonwebtoken')
 
-    signup : (req, res) => {
+const secretKey = 'secret key'
+module.exports = {
+    signup : (req, res, userdata) => {
         let user = req.body.username
         let password = req.body.password
-        if(user && password){
-            userdata.find({user}).toArray( (err, docs) => {
-                if(err) process.exit(1)
-                if(docs.length==0){
-                    userdata.insertOne({user,password}, (err, result) => {
-                        if(err) process.exit(1)
-                        if((1 == result.insertedCount))
+
+        if (user && password){
+            userdata.find({ user }).toArray( (err, docs) => {
+                if (err)
+                    return res.json({err})
+
+                if (docs.length==0){
+                    userdata.insertOne({ user, password }, (err, result) => {
+                        if (err)
+                            return res.json({err})
+                        if ((1 == result.insertedCount))
                             res.send('user registered')
                         else
                             res.sendStatus(500)
@@ -28,20 +30,42 @@ module.exports = {
             res.sendStatus(400)
     },
 
-    login : (req, res) => {
+    login : (req, res, userdata) => {
         let user = req.body.username
         let password = req.body.password
-        if(user && password){
-            userdata.find({user}).toArray( (err, docs) => {
-                if(err) process.exit(1)
-                if(docs.length==1 && docs[0].password==password)
-                    res.send('login successful')
+
+        if (user && password) {
+            userdata.find({ user }).toArray( (err, docs) => {
+                if (err)
+                    return res.json({err})
+
+                if (docs.length == 1 && docs[0].password == password){
+                    jwt.sign( {user, password}, secretKey, { expiresIn : '1m' }, (err, token) => {
+                        if (err)
+                            return res.json({err})
+                        res.cookie('token', token, {signed : true}).redirect(`/user/${user}`)
+                    })
+                }
                 else
                     res.status(401).send('incorrect username or password')
-                
               })
         }
         else
             res.sendStatus(400)
+    },
+
+    auth : (req, res, next) => {
+        let token = req.signedCookies.token
+        console.log(token);
+         
+        jwt.verify(token, secretKey, (err, decoded) => {
+            if(err)
+                return res.json({err})
+            next()
+        })
+    },
+
+    user : (req, res, blogdata) => {
+        res.send(`${req.params.user} logged in : user blogs will appear here`)
     }
 }
